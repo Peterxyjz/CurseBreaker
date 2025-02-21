1cc2e95a4ec00b807b5dec3cc712f9efd9b8f1bf
phongnqse172516@gmail.com <phongnqse172516@gmail.com>
2025-02-21 16:15:24 +0700

    test up

---
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,70 @@
+﻿using UnityEditor.Tilemaps;
+using UnityEngine;
+using System.Collections;
+public abstract class Enemy : MonoBehaviour
+{
+    [SerializeField] protected float enemyMoveSpeed = 1f;
+    [SerializeField] protected float enemyDamage = 1f;
+    protected PlayerController player; 
+    protected virtual void Start()
+    {
+        player = FindAnyObjectByType<PlayerController>();
+    }
+    public virtual void TakeDamage(float damage)
+    {
+        Die();
+    }
+    protected virtual void Die()
+    {
+        DisableColliders();
+        FadeOutAndDestroy();
+    }
+    protected virtual void HandleMovement()
+    {
+
+    }
+    protected virtual void FlipEnemy()
+    {
+        
+    }
+    private IEnumerator FadeOutAndDestroy()
+    {
+        // Lấy SpriteRenderer của đối tượng
+        SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+
+        // Kiểm tra nếu có SpriteRenderer
+        if (spriteRenderer != null)
+        {
+            // Đặt thời gian fade (2 giây)
+            float fadeDuration = 2f;
+            float timeElapsed = 0f;
+
+            // Lấy màu hiện tại của Sprite
+            Color startColor = spriteRenderer.color;
+
+            // Mờ dần trong 2 giây
+            while (timeElapsed < fadeDuration)
+            {
+                timeElapsed += Time.deltaTime;
+
+                // Tính toán giá trị alpha dựa trên thời gian
+                float alpha = Mathf.Lerp(1f, 0f, timeElapsed / fadeDuration);
+                spriteRenderer.color = new Color(startColor.r, startColor.g, startColor.b, alpha);
+
+                yield return null; // Đợi 1 frame
+            }
+        }
+
+        // Sau khi mờ dần, hủy đối tượng
+        Destroy(gameObject);
+    }
+    private void DisableColliders()
+    {
+        Collider2D[] colliders = GetComponents<Collider2D>();
+
+        foreach (var collider in colliders)
+        {
+            collider.enabled = false;
+        }
+    }
+}
---
+++ Assets/Scripts/Sword.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class Sword : MonoBehaviour
+{
+    private Transform playerTransform;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        void Start()
+        {
+            // Tìm Player trong scene theo tag (đảm bảo Player có tag "Player")
+            GameObject player = GameObject.FindGameObjectWithTag("Player");
+            if (player != null)
+            {
+                playerTransform = player.transform;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerTransform != null)
+        {
+            // Theo vị trí Player
+            transform.position = playerTransform.position;
+
+            // Theo hướng Player
+            float direction = playerTransform.localScale.x;
+            transform.localScale = new Vector3(direction, 1, 1);
+        }
+    }
+}
